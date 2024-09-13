4ad84b266958326bfb1d78d287e4dded0de7e3c1
subas-roy <subasroy46@gmail.com>
2024-09-13 18:23:02 +0600

    Add Icons and handle Show All jobs

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -3,6 +3,9 @@ import Job from "../Job/Job";
 
 const FeaturedJobs = () => {
   const[jobs, setJobs] = useState([]);
+  // this is the best way to load all data
+  const[dataLength, setDataLenhth] = useState([4]);
+    
   useEffect(() =>{
     fetch('jobs.json')
     .then(res => res.json())
@@ -16,8 +19,11 @@ const FeaturedJobs = () => {
         <p className="text-center">Explore thousands of job opportunities with all the information you need. Its your future</p>
         <div className="grid grid-cols-2 gap-6">
           {
-            jobs.map(job => <Job key={job.id} job={job}></Job>)
+            jobs.slice(0, dataLength).map(job => <Job key={job.id} job={job}></Job>)
           }
+        </div>
+        <div className={dataLength === jobs.length && 'hidden'}>
+          <button className="btn btn-primary" onClick={() => setDataLenhth(jobs.length)}>See All Jobs</button>
         </div>
       </div>
     </div>
--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,6 +1,8 @@
+import { MdOutlineLocationOn } from "react-icons/md";
+import { AiOutlineDollarCircle } from "react-icons/ai";
 
 const Job = ({job}) => {
-  const{logo, job_title, job_description, company_name, remote_or_onsite, job_type, salary, } = job;
+  const{logo, job_title, job_description, company_name, remote_or_onsite, job_type, salary, location} = job;
   return (
     <div className="card card-compact bg-base-100 shadow-xl">
       <figure>
@@ -14,6 +16,10 @@ const Job = ({job}) => {
         <div>
           <button className="px-5 py-2 border rounded font-extrabold mr-4">{remote_or_onsite}</button>
           <button className="px-5 py-2 border rounded font-extrabold mr-4">{job_type}</button>
+        </div>
+        <div className="mt-4 flex">
+          <h2 className="flex mr-4"><MdOutlineLocationOn className="text-2xl mr-2"></MdOutlineLocationOn> {location}</h2>
+          <h2 className="flex"><AiOutlineDollarCircle className="text-2xl mr-2"></AiOutlineDollarCircle> {salary}</h2>
         </div>
         <div className="card-actions">
           <button className="btn btn-primary">View Details</button>

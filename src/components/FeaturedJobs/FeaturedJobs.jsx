a67c9fe43088bd73d268bee895dd64381abf2125
subas-roy <subasroy46@gmail.com>
2024-09-13 15:40:18 +0600

    Set page layout and Display featured jobs

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -14,7 +14,7 @@ const FeaturedJobs = () => {
       <div>
         <h2 className="text-5xl text-center">Featured Jobs: {jobs.length}</h2>
         <p className="text-center">Explore thousands of job opportunities with all the information you need. Its your future</p>
-        <div>
+        <div className="grid grid-cols-2 gap-6">
           {
             jobs.map(job => <Job key={job.id} job={job}></Job>)
           }
--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,9 +1,24 @@
 
 const Job = ({job}) => {
-  const{logo} = job;
+  const{logo, job_title, job_description, company_name, remote_or_onsite, job_type, salary, } = job;
   return (
-    <div>
-      <img src={job.logo} alt="" />
+    <div className="card card-compact bg-base-100 shadow-xl">
+      <figure>
+        <img
+          src={logo}
+          alt="Job" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{job_title}</h2>
+        <p>{company_name}</p>
+        <div>
+          <button className="px-5 py-2 border rounded font-extrabold mr-4">{remote_or_onsite}</button>
+          <button className="px-5 py-2 border rounded font-extrabold mr-4">{job_type}</button>
+        </div>
+        <div className="card-actions">
+          <button className="btn btn-primary">View Details</button>
+        </div>
+      </div>
     </div>
   );
 };
